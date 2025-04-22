771f3c0b71e11a1dab972e0da103137a68f55df8
Sujan Banjara <banjara.suzan12@gmail.com>
2025-04-22 13:48:00 -0500

    Added file existence check before reading stories

--- Mod01/BedtimeStories/src/main/java/com/Mod01/BedtimeStories.java
+++ Mod01/BedtimeStories/src/main/java/com/Mod01/BedtimeStories.java
@@ -1,5 +1,6 @@
 package com.Mod01;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Scanner;
@@ -12,20 +13,28 @@ public class BedtimeStories {
             System.out.println("Enter the name of a Story: ");
             String fileName = scanner.nextLine();
 
-            FileInputStream fis = new FileInputStream("src/main/resources/DataFiles/" + fileName);
+            // just to make sure your file exist in fileName //
+            File file = new File(fileName);
+            if (!file.exists()){
+                System.out.println("Error: File not found!");
+                return;
+            }
 
+            FileInputStream fis = new FileInputStream(fileName);
             Scanner fileScanner = new Scanner(fis);
+            String line;
+            int lineNumber = 1;
 
             while (fileScanner.hasNextLine()){
-                String line = fileScanner.nextLine();
-                System.out.println(line);
+                line = fileScanner.nextLine();
+                System.out.println(lineNumber + " . " + line);
+                lineNumber++;
             }
 
             fileScanner.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         scanner.close();
 
     }
