0492ae2b31d7d4a8c53d30349dd6025715dcc8fe
Sujan Banjara <banjara.suzan12@gmail.com>
2025-04-22 11:45:48 -0500

    Create BedtimeStories.java to read story file using Scanner

--- Mod01/BedtimeStories/src/main/java/com/Mod01/BedtimeStories.java
+++ Mod01/BedtimeStories/src/main/java/com/Mod01/BedtimeStories.java
@@ -1,7 +1,32 @@
-package com.pluralsight;
+package com.Mod01;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class BedtimeStories {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter the name of a Story: ");
+            String fileName = scanner.nextLine();
+
+            FileInputStream fis = new FileInputStream("src/main/resources/DataFiles/" + fileName);
+
+            Scanner fileScanner = new Scanner(fis);
+
+            while (fileScanner.hasNextLine()){
+                String line = fileScanner.nextLine();
+                System.out.println(line);
+            }
+
+            fileScanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        scanner.close();
 
     }
 }
