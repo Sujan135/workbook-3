f1633a928f16570a9ce9338bd4d356e3402d2004
Sujan Banjara <banjara.suzan12@gmail.com>
2025-04-24 11:14:23 -0500

    Call logAction to record launch, search, and exit actions with timestamp

--- Mod03/SearchEngineLogger/src/main/java/com/pluralsight/SearchEngineLogger.java
+++ Mod03/SearchEngineLogger/src/main/java/com/pluralsight/SearchEngineLogger.java
@@ -1,20 +1,40 @@
 package com.pluralsight;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class SearchEngineLogger {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Application started");
+    private static void logAction(String action)  {
+        String filePath = "src/main/java/com/pluralsight/logs.txt";
+        try (FileWriter writer = new FileWriter(filePath, true)) {
+            String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+            writer.write(timestamp + " " + action + System.lineSeparator());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+public static void main(String[] args) {
+    logAction("launch");
+
+    System.out.println("Application started");
+    Scanner scanner = new Scanner(System.in);
 
         while(true) {
             System.out.println("Enter a search term (X to exit):");
             String input = scanner.nextLine().trim();
 
             if (input.equalsIgnoreCase("X")){
+                logAction("exit");
                 break;
             } else {
+                logAction("Search : " + input);
                 System.out.println("Searching for:" + input);
             }
         }
@@ -23,4 +43,6 @@ public class SearchEngineLogger {
         System.out.println("Application exited");
 
     }
+
+
 }
